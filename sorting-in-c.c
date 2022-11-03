9f6c1c442835c3e2ab5ef126c0518de614fb027d
uf0logic <ufologic@protonmail.com>
2022-11-04 00:02:52 +0300

    Added and tested the following sorting methods: 1)Bubble Sorting 2)Selection Sorting 3)Insertion Sorting
    Also added minor comfortablity functions (swap, printArray)

---
+++ sorting-in-c.c
@@ -0,0 +1,64 @@
+//
+// Created by Арсений Горячев on 03.11.2022.
+//
+
+#include <stdio.h>
+
+void printArray(int arr[], int size){
+     for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
+}
+
+int swap(int arr[], int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+void bubbleSort(int arr[], int size) {
+    int temp;
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = size - 1; j > i; --j) {
+            if (arr[j-1] > arr[j]) swap(arr, j, j-1);
+        }
+    }
+}
+
+void selectionSort(int arr[], int size) {
+    int index, temp;
+
+    for (int i = 0; i < size-1; ++i) {
+        index = i;
+        for (int j = i; j <= size - 1; ++j) {
+            if (arr[j] < arr[index]) index = j;
+        }
+        if (index != 1) swap(arr,i,index);
+    }
+}
+
+void insertionSort(int arr[], int size) {
+    int index, newElement;
+
+    for (int i = 1; i < size; ++i) {
+        newElement = arr[i];
+        index = i - 1;
+        while (index >= 0 && arr[index] > newElement) {
+            arr[index + 1] = arr[index];
+            index--;
+        }
+        arr[index + 1] = newElement;
+    }
+}
+
+int main() {
+    int arr[15] = {4,5,3,2,1,33,2,-5,12,8,3,234,5,-100,23,};
+
+    //bubbleSort(arr,15);
+    //selectionSort(arr,15);
+    //insertionSort(arr,15);
+
+    printArray(arr,15);
+
+
+
+    return 0;
+}
